f2fd2cfa729bad8a697c4a8271a9fb45a671ed84
Surigoma <Surigoma@users.noreply.github.com>
2025-06-29 16:15:13 +0900

    Add model define

--- src/define.hpp
+++ src/define.hpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
 
+#if defined(MODEL_M5STICK)
 #define IRIG_PIN GPIO_NUM_36
 #define JJY_PIN GPIO_NUM_26
-#define JJY1_PIN GPIO_NUM_0
 
 #define LED_PIN GPIO_NUM_10
+
+#else
+
+#error "Unsupported board model. Please define IRIG_PIN and JJY_PIN."
+
+#endif
